f3898f3d140b1d3bfd40d6e3c68d347dffe73003
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2021-06-24 14:40:59 -0500

    Update MinMaxSliderDrawer.cs
    Reduced the amount of repeated code so it is easier to understand/maintain for the future

--- Assets/SimpleMinMaxSlider/Scripts/Editor/MinMaxSliderDrawer.cs
+++ Assets/SimpleMinMaxSlider/Scripts/Editor/MinMaxSliderDrawer.cs
@@ -12,70 +12,43 @@ public class MinMaxSliderDrawer : PropertyDrawer {
 
         label.tooltip = minMaxAttribute.min.ToString("F2") + " to " + minMaxAttribute.max.ToString("F2");
 
+        bool isVector2 = propertyType == SerializedPropertyType.Vector2;
+        if (!isVector2 && propertyType != SerializedPropertyType.Vector2Int)
+        {
+            Debug.LogError(property.propertyPath + " is not combatible with MinMaxSlider Property Drawer!");
+            return;
+        }
+
         //PrefixLabel returns the rect of the right part of the control. It leaves out the label section. We don't have to worry about it. Nice!
         Rect controlRect = EditorGUI.PrefixLabel(position, label);
         
         Rect[] splittedRect = SplitRect(controlRect,3);
 
-        if(propertyType == SerializedPropertyType.Vector2){ 
+        EditorGUI.BeginChangeCheck();
 
-            EditorGUI.BeginChangeCheck();
+        Vector2 vector = isVector2 ? property.vector2Value : property.vector2IntValue;
+        float minVal = vector.x;
+        float maxVal = vector.y;
 
-            Vector2 vector = property.vector2Value;
-            float minVal = vector.x;
-            float maxVal = vector.y;
+        //F2 limits the float to two decimal places (0.00).
+        minVal = EditorGUI.FloatField(splittedRect[0], float.Parse(minVal.ToString("F2")));
+        maxVal = EditorGUI.FloatField(splittedRect[2], float.Parse(maxVal.ToString("F2")));
 
-            //F2 limits the float to two decimal places (0.00).
-            minVal = EditorGUI.FloatField(splittedRect[0], float.Parse(minVal.ToString("F2")));
-            maxVal = EditorGUI.FloatField(splittedRect[2], float.Parse(maxVal.ToString("F2")));
+        EditorGUI.MinMaxSlider(splittedRect[1], ref minVal, ref maxVal,
+        minMaxAttribute.min,minMaxAttribute.max);
 
-            EditorGUI.MinMaxSlider(splittedRect[1], ref minVal, ref maxVal,
-            minMaxAttribute.min,minMaxAttribute.max);
+        minVal = Mathf.Max(minMaxAttribute.min, minVal);
+        maxVal = Mathf.Min(minMaxAttribute.max, maxVal);
 
-            if(minVal < minMaxAttribute.min){
-                minVal = minMaxAttribute.min;
-            }
+        vector.x = minVal > maxVal ? maxVal : minVal;
+        vector.y = maxVal;
 
-            if(maxVal > minMaxAttribute.max){
-                maxVal = minMaxAttribute.max;
-            }
-
-            vector = new Vector2(minVal > maxVal ? maxVal : minVal, maxVal);
-
-            if(EditorGUI.EndChangeCheck()){
+        if(EditorGUI.EndChangeCheck()){
+            if(isVector2)
                 property.vector2Value = vector;
-            }
-
-        }else if(propertyType == SerializedPropertyType.Vector2Int){
-
-            EditorGUI.BeginChangeCheck();
-
-            Vector2Int vector = property.vector2IntValue;
-            float minVal = vector.x;
-            float maxVal = vector.y;
-
-            minVal = EditorGUI.FloatField(splittedRect[0], minVal);
-            maxVal = EditorGUI.FloatField(splittedRect[2], maxVal);
-
-            EditorGUI.MinMaxSlider(splittedRect[1], ref minVal, ref maxVal,
-            minMaxAttribute.min,minMaxAttribute.max);
-
-            if(minVal < minMaxAttribute.min){
-                maxVal = minMaxAttribute.min;
-            }
-
-            if(minVal > minMaxAttribute.max){
-                maxVal = minMaxAttribute.max;
-            }
-
-            vector = new Vector2Int(Mathf.FloorToInt(minVal > maxVal ? maxVal : minVal), Mathf.FloorToInt(maxVal));
-
-            if(EditorGUI.EndChangeCheck()){
-                property.vector2IntValue = vector;
-            }
-
-        } 
-
+            else
+                property.vector2IntValue = Vector2Int.RoundToInt(vector);
+        }
     }
 
     Rect[] SplitRect(Rect rectToSplit, int n){
